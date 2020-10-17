98882cf03747fec0d50b5ea67a23aa613f04b8a9
Son Dong Kyu <fkffksk20@gmail.com>
2020-10-18 00:44:13 +0900

    1

---
+++ 두 정수 사이의 합.py
@@ -0,0 +1,15 @@
+
+"""두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수,
+ solution을 완성하세요.
+예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다."""
+def solution(a, b):
+
+    c=((a+b))/2
+    if(c==a and c==b):
+       return a
+    answer = a+b+c
+    return answer
+
+a=5
+b=3
+print(solution(a,b))
